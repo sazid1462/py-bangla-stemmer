63f0fafe7a2b157ab267bde91f7985f03912c6c9
sazid <sazedul.islam@vivacomsolutions.com>
2019-04-12 15:04:16 +0600

    release v0.5.1

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ setup(
     author='Sazedul Islam',  # Type in your name
     author_email='sazidmailbox@gmail.com',  # Type in your E-Mail
     url='https://github.com/sazid1462/py-bangla-stemmer',  # Provide either the link to your github or to your website
-    download_url='https://github.com/sazid1462/py-bangla-stemmer/archive/0.5.1.tar.gz',  # I explain this later on
+    download_url='https://github.com/sazid1462/py-bangla-stemmer/archive/v0.5.1.tar.gz',  # I explain this later on
     keywords=['stemmer', 'bengali-stemmer', 'bangla-stemmer', 'rule-based-stemmer', 'nlp'],  # Keywords that define your package best
     install_requires=[  # I get to this in a second
     ],

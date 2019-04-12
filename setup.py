70393fc9660c620a713fa87451167cde109fb00a
sazid <sazedul.islam@vivacomsolutions.com>
2019-04-12 11:54:46 +0600

    releasing

---
+++ setup.py
@@ -0,0 +1,25 @@
+from distutils.core import setup
+
+setup(
+    name='py-bangla-stemmmer',  # How you named your package folder (MyLib)
+    packages=['si', 'si.pybs', 'si.pybs.resources', 'si.pybs.stemmer'],  # Chose the same as "name"
+    version='v0.1',  # Start with a small number and increase it with every change you make
+    license='GPL-3.0',  # Chose a license from here: https://help.github.com/articles/licensing-a-repository
+    description='Rule based Bengali Stemmer written in python',  # Give a short description about your library
+    author='Sazedul Islam',  # Type in your name
+    author_email='sazidmailbox@gmail.com',  # Type in your E-Mail
+    url='https://github.com/sazid1462/py-bangla-stemmer',  # Provide either the link to your github or to your website
+    download_url='https://github.com/sazid1462/py-bangla-stemmer/archive/v0.1.tar.gz',  # I explain this later on
+    keywords=['stemmer', 'bengali-stemmer', 'rule-based-stemmer', 'nlp'],  # Keywords that define your package best
+    install_requires=[  # I get to this in a second
+    ],
+    include_package_data=True,
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
+        'Intended Audience :: Developers',  # Define that your audience are developers
+        'Topic :: Software Development :: Natural Language Processing',
+        'License :: GPL License',  # Again, pick a license
+        'Programming Language :: Python :: 3',  # Specify which pyhton versions that you want to support
+    ],
+)
